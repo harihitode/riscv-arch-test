bc38e36acb3965607dc29d2817d01fe42349d589
Daichi Murakami <harihitode@gmail.com>
2021-06-23 17:46:09 +0900

    change model test for simulation

--- riscv-target/ladybug/model_test.h
+++ riscv-target/ladybug/model_test.h
@@ -1,70 +1,147 @@
-// RISC-V Compliance Test Header File
-// Copyright (c) 2017, Codasip Ltd. All Rights Reserved.
-// See LICENSE for license details.
-//
-// Description: Common header file for RV32I tests
+#ifndef _COMPLIANCE_MODEL_H
+#define _COMPLIANCE_MODEL_H
 
-#ifndef _COMPLIANCE_TEST_H
-#define _COMPLIANCE_TEST_H
+#define LADYBUG_CSR_WORK_ITEM_X 0xcd6
+#define LADYBUG_CSR_FORCE_QUIT  0xcd9
+#define LADYBUG_CSR_DUMP        0xcda
+#define LADYBUG_CSR_HEXDUMP     0xcdb
+#define MEMORY_MAPPED_O_ADDR    0x00300000
 
-#include "riscv_test.h"
+#define RVMODEL_DATA_SECTION                                            \
+        .pushsection .tohost,"aw",@progbits;                            \
+        .align 8; .global tohost; tohost: .dword 0;                     \
+        .align 8; .global fromhost; fromhost: .dword 0;                 \
+        .popsection;                                                    \
+        .align 8; .global begin_regstate; begin_regstate:               \
+        .word 128;                                                      \
+        .align 8; .global end_regstate; end_regstate:                   \
+        .word 4;
 
-//-----------------------------------------------------------------------
-// RV Compliance Macros
-//-----------------------------------------------------------------------
+// The .align 4 ensures that the signature begins at a 16-byte boundary
+#define RVMODEL_HALT                                              \
+  csrrsi x0, LADYBUG_CSR_FORCE_QUIT, 0x1;
 
-#define RV_COMPLIANCE_HALT                                                    \
-        RVTEST_PASS                                                           \
+//TODO: declare the start of your signature region here. Nothing else to be used here.
+// The .align 4 ensures that the signature ends at a 16-byte boundary
+#define RVMODEL_DATA_BEGIN                                              \
+  .align 4; .global begin_signature; begin_signature:
 
-#define RV_COMPLIANCE_RV32M                                                   \
-        RVTEST_RV32M                                                          \
+//TODO: declare the end of the signature region here. Add other target specific contents here.
+#define RVMODEL_DATA_END                                                      \
+  .align 4; .global end_signature; end_signature:                             \
+  RVMODEL_DATA_SECTION
 
-#define RV_COMPLIANCE_CODE_BEGIN                                              \
-        RVTEST_CODE_BEGIN_OLD                                                     \
+// There is no initialization for ladybug (empty)
+#define RVMODEL_IO_INIT
 
-#define RV_COMPLIANCE_CODE_END                                                \
-        RVTEST_CODE_END_OLD                                                       \
+// For code that has a split rom/ram area
+#define RVMODEL_BOOT      \
+  .section .text.init;    \
+  csrrw x1, LADYBUG_CSR_WORK_ITEM_X, x0;        \
+  csrrw x0, LADYBUG_CSR_DUMP, x1;                               \
+  LOCAL_IO_WRITE_STR("RISC-V Compliance Test for Ladybug.\n");
 
-#define RV_COMPLIANCE_DATA_BEGIN                                              \
-        RVTEST_DATA_BEGIN_OLD                                                     \
+// _SP = (volatile register)
+#define LOCAL_IO_WRITE_STR(_STR) RVMODEL_IO_WRITE_STR(x31, _STR)
+#define RVMODEL_IO_WRITE_STR(_SP, _STR)                                 \
+    .section .data.string;                                              \
+20001:                                                                  \
+    .string _STR;                                                       \
+    .section .text.init;                                                \
+    la a0, 20001b;                                                      \
+    jal FN_WriteStr;
 
-#define RV_COMPLIANCE_DATA_END                                                \
-        RVTEST_DATA_END_OLD                                                       \
+#define RSIZE 4
+// _SP = (volatile register)
+#define LOCAL_IO_PUSH(_SP)                                              \
+    la      _SP,  begin_regstate;                                       \
+    sw      ra,   (1*RSIZE)(_SP);                                       \
+    sw      t0,   (2*RSIZE)(_SP);                                       \
+    sw      t1,   (3*RSIZE)(_SP);                                       \
+    sw      t2,   (4*RSIZE)(_SP);                                       \
+    sw      t3,   (5*RSIZE)(_SP);                                       \
+    sw      t4,   (6*RSIZE)(_SP);                                      \
+    sw      s0,   (7*RSIZE)(_SP);                                      \
+    sw      a0,   (8*RSIZE)(_SP);
 
-#endif
-// RISC-V Compliance IO Test Header File
+// _SP = (volatile register)
+#define LOCAL_IO_POP(_SP)                                               \
+    la      _SP,   begin_regstate;                                      \
+    lw      ra,   (1*RSIZE)(_SP);                                       \
+    lw      t0,   (2*RSIZE)(_SP);                                       \
+    lw      t1,   (3*RSIZE)(_SP);                                       \
+    lw      t2,   (4*RSIZE)(_SP);                                       \
+    lw      t3,   (5*RSIZE)(_SP);                                       \
+    lw      t4,   (6*RSIZE)(_SP);                                       \
+    lw      s0,   (7*RSIZE)(_SP);                                       \
+    lw      a0,   (8*RSIZE)(_SP);
 
-/*
- * Copyright (c) 2005-2018 Imperas Software Ltd., www.imperas.com
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied.
- *
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- */
+//RVMODEL_IO_ASSERT_GPR_EQ
+// _SP = (volatile register)
+// _R = GPR
+// _I = Immediate
+// This code will check a test to see if the results
+// match the expected value.
+// It can also be used to tell if a set of tests is still running or has crashed
+// Test to see if a specific test has passed or not.  Can assert or not.
+#define RVMODEL_IO_ASSERT_GPR_EQ(_SP, _R, _I)                                 \
+    LOCAL_IO_PUSH(_SP)                                                  \
+    mv          s0, _R;                                                 \
+    li          t5, _I;                                                 \
+    beq         s0, t5, 20002f;                                         \
+    LOCAL_IO_WRITE_STR("Test Failed ");                              \
+    LOCAL_IO_WRITE_STR(": ");                                        \
+    LOCAL_IO_WRITE_STR(# _R);                                        \
+    LOCAL_IO_WRITE_STR("( ");                                        \
+    mv      a0, s0;                                                     \
+    jal FN_WriteNmbr;                                                   \
+    LOCAL_IO_WRITE_STR(" ) != ");                                    \
+    mv      a0, t5;                                                     \
+    jal FN_WriteNmbr;                                                   \
+    j 20003f;                                                           \
+20002:                                                                  \
+    LOCAL_IO_WRITE_STR("Test Passed ");                              \
+20003:                                                                  \
+    LOCAL_IO_WRITE_STR("\n");                                        \
+    LOCAL_IO_POP(_SP)
 
-#ifndef _COMPLIANCE_IO_H
-#define _COMPLIANCE_IO_H
+.section .text
+// FN_WriteStr: Add code here to write a string to IO
+// FN_WriteNmbr: Add code here to write a number (32/64bits) to IO
+FN_WriteStr:                             \
+csrrw t0, LADYBUG_CSR_WORK_ITEM_X, x0;   \
+bne t0, x0, FN_WriteStr_END;             \
+li a1, MEMORY_MAPPED_O_ADDR;             \
+FN_WriteStr_BEGIN:                       \
+lbu t0, 0x0(a0);                         \
+beq t0, x0, FN_WriteStr_END;             \
+sb t0, 0x0(a1);                          \
+addi a0, a0, 0x1;                        \
+j FN_WriteStr_BEGIN;                     \
+FN_WriteStr_END:                         \
+addi a0, x0, 0x0;                        \
+ecall;                                   \
+wfi;                                     \
+ret;                                     \
+FN_WriteNmbr:                            \
+csrrw x0, LADYBUG_CSR_DUMP, a0;          \
+ret;
 
-//-----------------------------------------------------------------------
-// RV IO Macros (Non functional)
-//-----------------------------------------------------------------------
+//RVTEST_IO_ASSERT_SFPR_EQ
+#define RVMODEL_IO_ASSERT_SFPR_EQ(_F, _R, _I)
+//RVTEST_IO_ASSERT_DFPR_EQ
+#define RVMODEL_IO_ASSERT_DFPR_EQ(_D, _R, _I)
 
-#define RVTEST_IO_INIT
-#define RVTEST_IO_WRITE_STR(_STR)
-#define RVTEST_IO_CHECK()
-#define RVTEST_IO_ASSERT_GPR_EQ(_R, _I)
-#define RVTEST_IO_ASSERT_SFPR_EQ(_F, _R, _I)
-#define RVTEST_IO_ASSERT_DFPR_EQ(_D, _R, _I)
+// TODO: specify the routine for setting machine software interrupt
+#define RVMODEL_SET_MSW_INT
 
-#endif // _COMPLIANCE_IO_H
+// TODO: specify the routine for clearing machine software interrupt
+#define RVMODEL_CLEAR_MSW_INT
+
+// TODO: specify the routine for clearing machine timer interrupt
+#define RVMODEL_CLEAR_MTIMER_INT
+
+// TODO: specify the routine for clearing machine external interrupt
+#define RVMODEL_CLEAR_MEXT_INT
+
+#endif // _COMPLIANCE_MODEL_H
